72a137b58e86e284934f2cce8572d229b157e126
learn <learn@learn05.local>
2017-08-30 16:30:39 -0700

    first commit

--- app.js
+++ app.js
@@ -32,6 +32,61 @@ app.get('/todo-list/:id', function(request, response){
       console.log(error);
     response.send("Error, couldn't fetch TodoList")
   })
+});
+
+app.post('/todo-list/:todoListId/todo/:id/complete', function(request, response){
+  Todo.findById(request.params.id).then(function(todo){
+    todo.isComplete = true
+    return todo.save()
+  }).then(function(todo){
+    response.redirect("/todo-list/" + request.params.todoListId)
+  }).catch(function(error){
+    response.send("Error, couldn't fetch Todo")
+  })
+});
+
+app.post('/todo-list/:todoListId/todo/new', function(request, response){
+  TodoList.findById(request.params.id).then(function(todoList){
+    return todoList.createTodo({
+      name: request.body.name,
+      isComplete: false
+    })
+    console.log(todoList);
+  }).then(function(todo){
+    response.redirect("/todo-list/" + request.params.todoListId)
+  }).catch(function(error){
+    response.send("Error, couldn't create Todo")
+  })
+})
+
+app.post('/todo-list/new', function(request, response){
+  TodoList.create({
+    name: request.body.name
+  }).then(function(todo){
+    response.redirect("/")
+  }).catch(function(error){
+    response.send("Error, couldn't create Todo List")
+  })
+})
+
+app.post('/todo-list/:todoListId/todo/:id/delete', function(request, response){
+  Todo.findById(request.params.id).then(function(todo){
+    return todo.destroy()
+  }).then(function(todo){
+    response.redirect("/todo-list/" + request.params.todoListId)
+  }).catch(function(error){
+    response.send("Error, couldn't fetch Todo")
+  })
+})
+
+app.post('/todo-list/:id/delete', function(request, response){
+  TodoList.findById(request.params.id).then(function(todoList){
+    return todoList.destroy()
+  }).then(function(todo){
+    response.redirect("/")
+  }).catch(function(error){
+    response.send("Error, couldn't fetch Todo")
+  })
 })
 
 app.listen(3000, function () {
